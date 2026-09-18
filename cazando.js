71c49f3eb445ab9c94dd0858cc74dfddf32f53e8
is1s2010 <isisnatalia2010@gmail.com>
2026-09-18 12:16:58 -0500

    poner constantes y variables

--- cazando.js
+++ cazando.js
@@ -1,7 +1,35 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO=50;
+const ANCHO_GATO=50;
+const ALTO_COMIDA=50;
+const ANCHO_COMIDA=50;
+
+
+function iniciarJuego(){
+
+    gatoX = 225; 
+    gatoY = 225;
+
+    comidaX = 450;
+    comidaY = 450;
+
+    graficarGato();
+    graficarComida();
+}
 
 function graficarGato(){
     ctx.fillStyle="pink";
-    ctx.fillRect(50,50,50,50)
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO)
+}
+
+
+function graficarComida(){
+    ctx.fillStyle="violet";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA)
 }
